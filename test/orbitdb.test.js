1cee5ed8d6a9dacf8238ae65a5eaab22cb270106
Hayden Young <haydenyoung@wijiti.com>
2023-12-12 15:54:09 +0000

    chore: Linting.

--- test/orbitdb.test.js
+++ test/orbitdb.test.js
@@ -8,22 +8,22 @@ import * as IPFS from 'ipfs-core'
 
 describe('Use DID Identity Provider with OrbitDB', function () {
   this.timeout(10000)
-  
+
   const didStr = 'did:key:z6MkpnTJwrrVuphNh1uKb5DB7eRxvqniVaSDUHU6jtGVmn3r'
-  
+
   let ipfs
   let orbitdb
   let didProvider
   let provider
-  
+
   beforeEach(async () => {
     ipfs = await IPFS.create({})
 
     const seed = new Uint8Array([157, 94, 116, 198, 19, 248, 93, 239, 173, 82, 245, 222, 199, 7, 183, 177, 123, 238, 83, 240, 143, 188, 87, 191, 33, 95, 58, 136, 46, 218, 219, 245])
-    
+
     OrbitDBIdentityProviderDID.setDIDResolver(KeyDidResolver.getResolver())
     useIdentityProvider(OrbitDBIdentityProviderDID)
-    
+
     didProvider = new Ed25519Provider(seed)
     provider = OrbitDBIdentityProviderDID({ didProvider })
   })
